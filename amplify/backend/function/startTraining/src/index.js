58ec1a7313e61f99ffb3fb8191ad8d14b17eb3f3
reptiliano37 <miguel.arara5@gmail.com>
2022-01-12 18:33:56 +0100

    Creation of function that will start a new training

---
+++ amplify/backend/function/startTraining/src/index.js
@@ -0,0 +1,12 @@
+/* Amplify Params - DO NOT EDIT
+	API_PADDELAPP_GRAPHQLAPIENDPOINTOUTPUT
+	API_PADDELAPP_GRAPHQLAPIIDOUTPUT
+	API_PADDELAPP_GRAPHQLAPIKEYOUTPUT
+	ENV
+	REGION
+Amplify Params - DO NOT EDIT */
+
+exports.handler = async (event) => {
+    console.log(event)
+    return true;
+};
