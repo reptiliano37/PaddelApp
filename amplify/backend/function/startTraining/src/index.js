f5ebc2a0f6e01ac9ae73f0874e3875581f706858
reptiliano37 <miguel.arara5@gmail.com>
2022-01-14 11:37:53 +0100

    Adding libraries to startTraining function and type mutation created

--- amplify/backend/function/startTraining/src/index.js
+++ amplify/backend/function/startTraining/src/index.js
@@ -6,7 +6,26 @@
 	REGION
 Amplify Params - DO NOT EDIT */
 
+const appsync = require("aws-appsync");
+const gql = require("graphql-tag");
+require("cross-fetch/polyfill");
+
 exports.handler = async (event) => {
-    console.log(event)
+    const graphqlClient = new appsync.AWSAppSyncClient({
+        url: process.env.API_PADDELAPP_GRAPHQLAPIENDPOINTOUTPUT,
+        region: process.env.REGION,
+        auth: {
+            type: "AWS_IAM",
+            credentials: {
+                accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+                secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+                sessionToken: process.env.AWS_SESSION_TOKEN
+            }
+        },
+        disableOffline: true
+    });
+
+    const trainer = event.identity.username;
+    const invitee = event.arguments.invitee;
     return true;
 };
